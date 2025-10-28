e610190f7c99cf03d4ec2af64a9111f01035de70
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2025-10-28 11:43:36 +0530

    view all courses, view selected course and delete course by there instructor only is implimented

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -48,4 +48,81 @@ const addCourse = async (req, res, next) => {
   }
 };
 
-export { addCourse };
+const allCourse = async (req, res, next) => {
+  try {
+    const allCourses = await prisma.Course.findMany({
+      include: {
+        instructor: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (allCourses.length === 0)
+      return res.status(400).json({ message: "No course added yet..!!!" });
+
+    res.status(200).json({ message: "All Courses", allCourses });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const selectedCourse = async (req, res, next) => {
+  try {
+    let id = Number(req.params.id);
+
+    let selectedCourse = await prisma.Course.findUnique({
+      where: { id },
+      include: {
+        instructor: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (!selectedCourse)
+      return res.status(404).json({ message: "Course not found..!!!" });
+
+    res.status(200).json({ message: "Course Details", selectedCourse });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteCourse = async (req, res, next) => {
+  try {
+    if (req.user.role !== "instructor")
+      return res.status(404).json({ message: "Unauthorize Access..!!!" });
+
+    const id = Number(req.params.id);
+
+    const course = await prisma.Course.findUnique({
+      where: { id },
+    });
+    if (!course)
+      return res
+        .status(404)
+        .json({ message: "The course you want to delete does not exist..!!" });
+
+    if (course.instructorId !== Number(req.user.id))
+      return res.status(400).json({
+        message: "Deletion failed you are not the instructor of this course",
+      });
+
+    let deletedCourse = await prisma.Course.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: "Course Deleted..!!", deletedCourse });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { addCourse, allCourse, selectedCourse, deleteCourse };
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { addCourse } from '../controllers/courseController.js';
+import { addCourse, allCourse, selectedCourse, deleteCourse } from '../controllers/courseController.js';
 import authenticate from '../../middlewares/authMiddleware.js';
 
 router.post('/add',authenticate,addCourse);
+router.get('/viewall', allCourse)
+router.get('/:id/details', selectedCourse)
+router.delete('/:id/delete',authenticate, deleteCourse)
 
 export default router;
